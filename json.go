ee7dcea14144b10c9543123f3833e26201fef716
Dominik Nowak <dnowak94@live.de>
2024-08-14 17:04:02 +0200

    Fix CWE-703 in json.go

--- json.go
+++ json.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
 func respondWithError(w http.ResponseWriter, code int, msg string) {
 	if code > 499 {
 		log.Printf("Responding with 5XX error: %s", msg)
-	}
-	type errorResponse struct {
-		Error string `json:"error"`
 	}
 	respondWithJSON(w, code, errorResponse{
 		Error: msg,
@@ -19,13 +21,15 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json")
 	dat, err := json.Marshal(payload)
 	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
 		w.WriteHeader(500)
-		return
+		log.Fatal(fmt.Errorf("Error marshalling JSON: %s", err).Error())
 	}
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	w.Write(dat)
+	code, err = w.Write(dat)
+	if err != nil {
+		log.Fatal("sending Response failed with error: %s", err)
+	}
 }
